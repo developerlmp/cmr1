6075b3a2063defd9b5bcae57dd34cc731fc90973
developerlmp <leminhphuc1804@gmail.com>
2022-04-11 00:04:32 +0700

    Dynamic URL Routing & Templates

--- accounts/views.py
+++ accounts/views.py
@@ -19,5 +19,9 @@ def products(request):
     products=Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
 
-def customers(request):
-    return render(request,'accounts/customer.html')
+def customers(request,pk_test):
+    customer=Customer.objects.get(id=pk_test)
+    orders=customer.order_set.all()
+    order_count = orders.count()
+    context={'customer':customer,'orders':orders,'order_count':order_count}
+    return render(request,'accounts/customer.html',context)
