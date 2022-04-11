f4d50166fdf51822a62d917c3f52a4a740d2abec
developerlmp <leminhphuc1804@gmail.com>
2022-04-11 23:15:40 +0700

    Inline Formsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from matplotlib.style import context
 # Create your views here.
 
@@ -28,15 +29,18 @@ def customer(request,pk_test):
     context={'customer':customer,'orders':orders,'order_count':order_count}
     return render(request,'accounts/customer.html',context)
 
-def createOrder(request):
-    form=OrderForm()
+def createOrder(request,pk):
+    OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer=Customer.objects.get(id=pk)
+    formset=OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #form=OrderForm(initial={'customer':customer})
     if request.method=='POST':
         #print('Printing POST:', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
-    context={'form':form}
+    context={'form':formset}
     return render(request,'accounts/order_form.html',context)
 
 def updateOrder(request,pk):
