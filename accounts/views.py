5093e96f1d03edd49dd4cb9a2a3114707a9d8e45
developerlmp <leminhphuc1804@gmail.com>
2022-04-13 21:16:32 +0700

    User Profile With One To One Relationship

--- accounts/views.py
+++ accounts/views.py
@@ -28,7 +28,7 @@ def registerPage(request):
                 
                 group=Group.objects.get(name='customer')
                 user.groups.add(group)
-                
+                Customer.objects.create(user=user,name=user.username)
                 messages.success(request,'Account was created for '+ username)
 
                 return redirect('login')
@@ -69,8 +69,14 @@ def home(request):
     'delivered':delivered,'pending':pending}
     return render(request,'accounts/dashboard.html',context)
 
+@login_required(login_url='login')  
+@allowed_users(allowed_roles=['customer']) 
 def userPage(request):
-    context={}
+    orders=request.user.customer.order_set.all()
+    total_orders=orders.count()
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status='Pending').count()
+    context={'orders':orders, 'total_orders':total_orders,'delivered':delivered,'pending':pending}
     return render(request,'accounts/user.html',context)
 
 @login_required(login_url='login')  
